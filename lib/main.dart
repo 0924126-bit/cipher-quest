6bb8b6cf84a002e35b215ddd0119f071319ad0ab
y8*******@gm***.com <y85822312@gmail.com>
2026-08-22 02:18:12 +0000

    feat(flutter): add /chaser /cursed /hunter routes with fade transitions

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'dashboard/dashboard_page.dart';
 import 'decoder/decoder_page.dart';
+import 'roles/chaser_page.dart';
+import 'roles/cursed_page.dart';
+import 'roles/hunter_page.dart';
 import 'theme/app_theme.dart';
 
 void main() {
@@ -11,6 +14,9 @@ void main() {
 /// Routes:
 ///   /              -> operator dashboard
 ///   /machine/:id   -> decoder page (full-screen)
+///   /chaser        -> chaser alarm page (security buzzer)
+///   /cursed        -> cursed one-shot curse button page
+///   /hunter        -> hunter phone notification terminal
 class CipherQuestApp extends StatelessWidget {
   const CipherQuestApp({super.key});
 
@@ -30,6 +36,31 @@ class CipherQuestApp extends StatelessWidget {
           return PageRouteBuilder(
             settings: settings,
             pageBuilder: (_, __, ___) => DecoderPage(machineId: id),
+            transitionsBuilder: (_, anim, __, child) =>
+                FadeTransition(opacity: anim, child: child),
+            transitionDuration: const Duration(milliseconds: 400),
+          );
+        }
+
+        Widget? rolePage;
+        if (segs.length == 1) {
+          switch (segs[0]) {
+            case 'chaser':
+              rolePage = const ChaserPage();
+              break;
+            case 'cursed':
+              rolePage = const CursedPage();
+              break;
+            case 'hunter':
+              rolePage = const HunterPage();
+              break;
+          }
+        }
+        if (rolePage != null) {
+          final page = rolePage;
+          return PageRouteBuilder(
+            settings: settings,
+            pageBuilder: (_, __, ___) => page,
             transitionsBuilder: (_, anim, __, child) =>
                 FadeTransition(opacity: anim, child: child),
             transitionDuration: const Duration(milliseconds: 400),
