014da33ec6d564ebe046732b2a92dcfd2600498e
y8*******@gm***.com <y85822312@gmail.com>
2026-08-22 02:08:20 +0000

    feat(flutter): alarm service non-web stub

---
+++ lib/services/alarm_service_stub.dart
@@ -0,0 +1,11 @@
+/// Non-web stub for the alarm / curse sound synthesizer.
+///
+/// The festival runs everything in browsers; native platforms just no-op.
+class AlarmBackend {
+  void startSiren() {}
+  void stopSiren() {}
+  void playCurseSting() {}
+  void playCursePress() {}
+  void vibrate() {}
+  void dispose() {}
+}
